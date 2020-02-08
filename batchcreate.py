4073b91c1c776c5a1f8253ded9d6a3f1bb76f90e
grecoe <grecoe@microsoft.com>
2020-02-08 14:06:16 -0500

    Try/Except around full apps in case of errors we might detect it.

--- batchcreate.py
+++ batchcreate.py
@@ -16,64 +16,69 @@ from scripts.general_utils import JobType, JobLog
 
 job_log = JobLog(JobType.batch_scoring)
 
-'''
-    Get the program arguments and user authentication into the context
-'''
-job_log.startStep("Setup")
-programargs = loadConfiguration(ExperimentType.batch_scoring,sys.argv[1:])
-userAuth = get_auth()
-program_context = BatchScoringContext(programargs, userAuth)
-job_log.endStep("Setup")
+try :
+    '''
+        Get the program arguments and user authentication into the context
+    '''
+    job_log.startStep("Setup")
+    programargs = loadConfiguration(ExperimentType.batch_scoring,sys.argv[1:])
+    userAuth = get_auth()
+    program_context = BatchScoringContext(programargs, userAuth)
+    job_log.endStep("Setup")
 
 
-'''
-    Get a workspace
-'''
-job_log.startStep("Workspace")
-program_context.generateWorkspace()
-job_log.endStep("Workspace")
+    '''
+        Get a workspace
+    '''
+    job_log.startStep("Workspace")
+    program_context.generateWorkspace()
+    job_log.endStep("Workspace")
 
 
-'''
-    Deal with storage details
-'''
-job_log.startStep("Storage Containers")
-program_context.generateStorageContainers()
-job_log.endStep("Storage Containers")
+    '''
+        Deal with storage details
+    '''
+    job_log.startStep("Storage Containers")
+    program_context.generateStorageContainers()
+    job_log.endStep("Storage Containers")
 
-'''
-    Upload data files
-'''
-job_log.startStep("File Uploads")
-program_context.uploadDataFiles()
-job_log.endStep("File Uploads")
+    '''
+        Upload data files
+    '''
+    job_log.startStep("File Uploads")
+    program_context.uploadDataFiles()
+    job_log.endStep("File Uploads")
 
-'''
-    Get or create batch compute
-'''
-job_log.startStep("Batch Compute")
-program_context.generateCompute()
-job_log.endStep("Batch Compute")
+    '''
+        Get or create batch compute
+    '''
+    job_log.startStep("Batch Compute")
+    program_context.generateCompute()
+    job_log.endStep("Batch Compute")
 
-'''
-    Create the datasets for the pipeline
-'''
-job_log.startStep("Data References")
-program_context.createPipelineDataReferences()
-job_log.endStep("Data References")
+    '''
+        Create the datasets for the pipeline
+    '''
+    job_log.startStep("Data References")
+    program_context.createPipelineDataReferences()
+    job_log.endStep("Data References")
 
-'''
-    Create the pipeline
-'''
-job_log.startStep("Pipeline Creation")
-program_context.createPipeline()
-job_log.endStep("Pipeline Creation")
+    '''
+        Create the pipeline
+    '''
+    job_log.startStep("Pipeline Creation")
+    program_context.createPipeline()
+    job_log.endStep("Pipeline Creation")
 
 
-'''
-    Add in final details and dump the log
-'''
-job_log.addInfo("Pipeline Status: {}".format(program_context.publishedPipeline.status))
-job_log.addInfo("Pipeline Endpoint: {}".format(program_context.publishedPipeline.endpoint))
+    '''
+        Add in final details and dump the log
+    '''
+    job_log.addInfo("Pipeline Status: {}".format(program_context.publishedPipeline.status))
+    job_log.addInfo("Pipeline Endpoint: {}".format(program_context.publishedPipeline.endpoint))
+    
+except Exception as ex:
+    job_log.addInfo("An error occured executing this path")
+    job_log.addInfo(str(ex))
 
 job_log.dumpLog()
--- rtscreate.py
+++ rtscreate.py
@@ -22,75 +22,80 @@ from scripts.general_utils import JobType, JobLog
 
 job_log = JobLog(JobType.real_time_scoring)
 
-'''
-    Get the program arguments and user authentication into the context
-'''
-job_log.startStep("Setup")
-programargs = loadConfiguration(ExperimentType.real_time_scoring,sys.argv[1:])
-userAuth = get_auth()
-program_context = RealTimeScoringContext(programargs, userAuth)
-job_log.endStep("Setup")
+try:
+    '''
+        Get the program arguments and user authentication into the context
+    '''
+    job_log.startStep("Setup")
+    programargs = loadConfiguration(ExperimentType.real_time_scoring,sys.argv[1:])
+    userAuth = get_auth()
+    program_context = RealTimeScoringContext(programargs, userAuth)
+    job_log.endStep("Setup")
 
 
-'''
-    Get a workspace
-'''
-job_log.startStep("Workspace")
-program_context.generateWorkspace()
-job_log.endStep("Workspace")
+    '''
+        Get a workspace
+    '''
+    job_log.startStep("Workspace")
+    program_context.generateWorkspace()
+    job_log.endStep("Workspace")
 
-'''
-    Get or create an experiment
-'''
-job_log.startStep("Experiment")
-program_context.generateExperiment()
-job_log.endStep("Experiment")
+    '''
+        Get or create an experiment
+    '''
+    job_log.startStep("Experiment")
+    program_context.generateExperiment()
+    job_log.endStep("Experiment")
 
-'''
-    Get existing or create and register model
-'''
-job_log.startStep("Model")
-program_context.generateModel()
-job_log.endStep("Model")
+    '''
+        Get existing or create and register model
+    '''
+    job_log.startStep("Model")
+    program_context.generateModel()
+    job_log.endStep("Model")
 
-'''
-    Create or update the container image
-'''
-job_log.startStep("Container Image")
-if program_context.loadImage() == False:
-    program_context.generateImage()
-    program_context.testImage()
-job_log.endStep("Container Image")
+    '''
+        Create or update the container image
+    '''
+    job_log.startStep("Container Image")
+    if program_context.loadImage() == False:
+        program_context.generateImage()
+        program_context.testImage()
+    job_log.endStep("Container Image")
 
-'''
-    Create/attach existing compute target
+    '''
+        Create/attach existing compute target
 
-    To attach, you have to provide the cluster name and resource group name
-    in the program arguments. By default they are set to None so that a new cluster
-    is generated.
-'''
-job_log.startStep("Compute Target")
-program_context.generateComputeTarget(
-     cluster_name = program_context.programArguments.aks_existing_cluster,
-     resource_group = program_context.programArguments.aks_existing_rg
-     )
-job_log.endStep("Compute Target")
+        To attach, you have to provide the cluster name and resource group name
+        in the program arguments. By default they are set to None so that a new cluster
+        is generated.
+    '''
+    job_log.startStep("Compute Target")
+    program_context.generateComputeTarget(
+         cluster_name = program_context.programArguments.aks_existing_cluster,
+         resource_group = program_context.programArguments.aks_existing_rg
+         )
+    job_log.endStep("Compute Target")
 
-'''
-    Create service
-'''
-job_log.startStep("Web Service")
-program_context.generateWebService()
+    '''
+        Create service
+    '''
+    job_log.startStep("Web Service")
+    program_context.generateWebService()
 
-print(program_context.webserviceapi)
-if program_context.webserviceapi:
-    for key in program_context.webserviceapi.keys():
-        job_log.addInfo("{} - {}".format(key, program_context.webserviceapi[key] ))
-job_log.endStep("Web Service")
+    print(program_context.webserviceapi)
+    if program_context.webserviceapi:
+        for key in program_context.webserviceapi.keys():
+            job_log.addInfo("{} - {}".format(key, program_context.webserviceapi[key] ))
+    job_log.endStep("Web Service")
 
-job_log.startStep("Web Service Test")
-program_context.testWebService()
-job_log.endStep("Web Service Test")
+    job_log.startStep("Web Service Test")
+    program_context.testWebService()
+    job_log.endStep("Web Service Test")
+
+except Exception as ex:
+    job_log.addInfo("An error occured executing this path")
+    job_log.addInfo(str(ex))
 
 job_log.dumpLog()
 
